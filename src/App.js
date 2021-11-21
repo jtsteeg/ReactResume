9647b57173a28ffc7cc956d346601a7f3385cf9b
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-11-21 17:39:30 -0600

    adds components to be used in resume structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import styles from './App.module.css';
+import Title from './Title/Title';
+import Background from './Background/Background';
+import Picture from './Picture/Picture';
+import Experience from './Experience/Experience';
+import CertsEducation from './Certs+Education/Certs+Education';
 
 function App() {
   return (
     <div className={styles.App}>
-
+      <Title />
+      <Background />
+      <Picture />
+      <Experience />
+      <CertsEducation />
     </div>
   );
 }
