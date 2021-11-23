405f1619394d2bcdb15b6deb644dc6430ea2a00a
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-11-23 14:38:34 -0600

    fixes img paths broken by GH page deployment

--- src/Picture/Picture.js
+++ src/Picture/Picture.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './Picture.module.css';
+import profilePic from '../Images/pfp2.png';
+import secretImage from '../Images/cowboys.png';
+
 
 export default class App extends React.Component {
   render() {
     return (
       <div className={styles.parent}>
-        <img src="/pfp2.png" alt="Jacob Steeg (right)" className={styles.img}/>
-        <img src="/cowboys.png" alt="secret" className={styles.img2}/>
+        <img src={profilePic} alt="Jacob Steeg (right)" className={styles.img}/>
+        <img src={secretImage} alt="secret" className={styles.img2}/>
       </div>
     );
   }
