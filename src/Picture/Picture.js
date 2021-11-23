fd821d9dd0d91a85799b0b396030ca5a390c5e48
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-11-23 15:45:54 -0600

    changes secret image, fixes image source path to point to public folder

--- src/Picture/Picture.js
+++ src/Picture/Picture.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import styles from './Picture.module.css';
-import profilePic from '../Images/pfp2.png';
-import secretImage from '../Images/cowboys.png';
 
 
 export default class App extends React.Component {
   render() {
     return (
       <div className={styles.parent}>
-        <img src={profilePic} alt="Jacob Steeg (right)" className={styles.img}/>
-        <img src={secretImage} alt="secret" className={styles.img2}/>
+        <img src={process.env.PUBLIC_URL + '/pfp2.png'}  alt="Jacob Steeg (right)" className={styles.img}/>
+        <img src={process.env.PUBLIC_URL + '/frog.png'} alt="secret" className={styles.img2}/>
       </div>
     );
   }
