24290733700387b3db596fe5bd201d40c86ae6b7
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-11-22 22:09:01 -0600

    adds animation to profile picture

--- src/Picture/Picture.js
+++ src/Picture/Picture.js
@@ -4,8 +4,9 @@ import styles from './Picture.module.css';
 export default class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className={styles.parent}>
         <img src="/pfp2.png" alt="Jacob Steeg (right)" className={styles.img}/>
+        <img src="/cowboys.png" alt="secret" className={styles.img2}/>
       </div>
     );
   }
