8d149e204fbfe716581055468abf72fbbb3fffef
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-11-22 19:34:34 -0600

    configures background component

--- src/Background/Background.js
+++ src/Background/Background.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './Background.module.css';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Background</h1>
+        <div className={styles.background}></div>
       </div>
     );
   }
