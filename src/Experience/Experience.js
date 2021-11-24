56c68e081eaca47c7efbcff40e84da24afde2e1d
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-11-23 19:11:20 -0600

    updates content

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -7,28 +7,28 @@ export default class App extends React.Component {
       <div className={styles.experience}>
         <h1> Experience: </h1>
         <h3> Microsoft</h3>
-        <h4> Data Center Technician </h4>
+        <h4> Datacenter Technician </h4>
             <p class="date"> 2017 - Current. Promoted to Senior in July 2021.</p>
             <ul>
-                <li> Coordinates safety and security compliance across multiple shifts in a 24/7 environment.</li>
-                <li> Performs shift-based tactical management tasks such as prioritizing ticketing queues, working to resolve escalations, and clearing blocking issues to enable continued delivery of datacenter support activities.</li>
-                <li> Performs cable installations, hardware installations, testing, and troubleshooting using standard datacenter processes.</li>
-                <li> Assists Datacenter Inventory and Asset Management Team with shipping/receiving and inventory management to address high work volumes.</li>
+                <li> Ensures Microsoft Visio is installed on all production equipment.</li>
+                <li> Responsible for 100% increase of bees in the workplace.</li>
+                <li> Personally taste tests all cabling prior to installation</li>
+                <li> Rebuilt company toaster to run AI powered blockchain</li>
             </ul>
             <h3> Atos</h3>
-            <h4> Data Center Technician Lead</h4>
+            <h4> Datacenter Technician Lead</h4>
             <p class="date"> 2015 - 2017</p>
             <ul>
-                <li> Plan, manage and communicate end-to-end project plans with Client and Vendor Teams.</li>
-                <li> Encourage team to share lessons learned, best practices, and innovations on process/procedures and tools to improve efficiencies and productivity.</li>
-                <li> Monitor and control daily service ticket activity, customer calls and service level agreements.</li>
+                <li> Supervised and developed a team of 4-5 feral wallabies.</li>
+                <li> Successfully accomplished lunch every day</li>
+                <li> Messaged Steve Wozniak on linkedIn, Steve if your reading this please respond re: my app idea  </li>
             </ul>
             <h3> Egltech</h3>
             <h4> Helpdesk Support Technician</h4>
             <p class="date"> 2014 - 2015</p>
             <ul>
-                <li> Deployed OS and essential software on client workstations.</li>
-                <li> Provided remote support to clients over the help desk with Kaseya and Bomgar.</li>
+                <li> Deployed OS and all sorts of stuff on client workstations.</li>
+                <li> Provided remote support to clients using cryptic, passive-aggressive telegrams.</li>
             </ul>        
       </div>
     );
